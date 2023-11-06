ad16cb6479dfe369e5ed52756cad6cdd5e1e722a
Santiago Viana <salviega6@gmail.com>
2023-11-06 16:59:14 -0500

    :bug: Extra ,

--- contracts/UNI.sol
+++ contracts/UNI.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/token/ERC777/ERC777.sol';
 
- /**
+/**
  * @title  UNI Token
  * @dev This contract allows to withdraw UNI tokens
  * @notice The owner is only allowed to mint tokens
@@ -20,7 +20,7 @@ contract UNI is ERC777, Ownable {
 	constructor(
 		string memory _name,
 		string memory _symbol,
-		address[] memory _defaultOperators,
+		address[] memory _defaultOperators
 	) ERC777(_name, _symbol, _defaultOperators) {}
 
 	/** @notice mints UNI tokens
